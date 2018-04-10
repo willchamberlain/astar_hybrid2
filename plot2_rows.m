b17440f63dd7ee2e6719844dcb333f88f8d4cf4b
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-04-10 15:03:17 +1000

    return graphics object handle

--- plot2_rows.m
+++ plot2_rows.m
@@ -1,3 +1,3 @@
-function plot2_rows(matrix_of_2D, varargin)
-plot(matrix_of_2D(1,:) , matrix_of_2D(2,:) , varargin{:})
+function graphics_object_handle__ = plot2_rows(matrix_of_2D, varargin)
+    graphics_object_handle__  =  plot(matrix_of_2D(1,:) , matrix_of_2D(2,:) , varargin{:});
 end
