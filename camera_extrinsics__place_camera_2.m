d5775a0a73f65a5f3702082b90716df6bc1d6760
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-05-09 20:58:20 +1000

    use camera_rdf_coordinate_system to point the camera at a target point

--- camera_extrinsics__place_camera_2.m
+++ camera_extrinsics__place_camera_2.m
@@ -33,16 +33,28 @@ function camera__ = camera_extrinsics__place_camera_2(min_angle_degs_,angle_rang
 %     disp(cam_pose_xyz)
 %     camera__ = camera.move(  rot_trans_to_SE3_hom(eye(3), cam_pose_xyz)  );
 %     camera__ = camera.move(  [   [ rot_SO3, cam_pose_xyz ] ; [ 0 0 0 1 ]   ]  );  % :-- move in world coordinate system ( FLU ) : camera is aligned to 
-%     
+% %     
+
+        
+    cam_direction_vector = target_point_ - cam_pose_xyz ;
+    cam_rdf_coord_sys = camera_rdf_coordinate_system(cam_direction_vector, [ cam_direction_vector(1:2) ; cam_direction_vector(3)+2 ])  ;   %  vector_along_x_axis_ , vector_in_z_axis_plane_ )
+    cam_T =  rt2tr(  cam_rdf_coord_sys , cam_pose_xyz)  ;
+    display('camera_extrinsics__place_camera_2: LOCKED VERTICAL')
     camera__ = CentralCamera('default');
-    display(camera__.C)   ;
-    camera__ = camera__.move(  rot_trans_to_SE3_hom(eye(3), cam_pose_xyz)  );
-    display(camera__.C)   ;
-%     camera__ = camera__.move(  [   [ rotx(degtorad(-7))*roty(degtorad(30)) *  rotz(degtorad(35)), [ 0 0 0  ]' ] ; [ 0 0 0 1 ]   ]  );
-    camera__ = camera__.move(  [   [ rotx(degtorad(-7))*roty(degtorad(90)) *  rotz(degtorad(0)), [ 0 0 0  ]' ] ; [ 0 0 0 1 ]   ]  );
-    display(camera__.C)   ;
-    camera__ = camera__.move(  [   [ roty(degtorad(90))*rotz(degtorad(-90)) , [ 0 0 0  ]' ] ; [ 0 0 0 1 ]   ]  );
-    display(camera__.C)   ;
-    
+    camera__.T = cam_T  ;
+
+%     camera__ = CentralCamera('default');
+%     display(camera__.C)   ;
+%     camera__ = camera__.move(  rot_trans_to_SE3_hom(eye(3), cam_pose_xyz)  );
+%     display(camera__.C)   ;
+% %     camera__ = camera__.move(  [   [ rotx(degtorad(-7))*roty(degtorad(30)) *  rotz(degtorad(35)), [ 0 0 0  ]' ] ; [ 0 0 0 1 ]   ]  );
+%     camera__ = camera__.move(  [   [ rotx(degtorad(-7))*roty(degtorad(90)) *  rotz(degtorad(0)), [ 0 0 0  ]' ] ; [ 0 0 0 1 ]   ]  );
+%     display(camera__.C)   ;
+%     camera__ = camera__.move(  [   [ roty(degtorad(90))*rotz(degtorad(-90)) , [ 0 0 0  ]' ] ; [ 0 0 0 1 ]   ]  );
+%     display(camera__.C)   ;
+%     
+%         camera_rdf_coord_sys = camera_rdf_coordinate_system( cam_direction_vector, [ cam_direction_vector(1:2) ; cam_direction_vector(3)+2 ] )  ;
+%         cam_T =  rt2tr(  camera_rdf_coord_sys , cam_position)  ;
+%         cam_array(ii_+1).plot_camera('Tcam',cam_T,'scale', 0.20)  ;        %  note: _try_ this        
     
 end    
