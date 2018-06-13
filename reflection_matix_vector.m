3c52e630aa2024e8320ff24e62f070c01a1036bc
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-06-13 13:11:52 +1000

    Functions to generate reflection transforms (SO3) from a plane defined by two vectors plus an origin, or by a plane normal vector plus an origin.

---
+++ reflection_matix_vector.m
@@ -0,0 +1,14 @@
+function reflection_matrix_SO3__ = reflection_matix_vector(vector_to_reflect_against_, reflection_origin_)
+% vector_to_reflect_against_ :  vector to reflect against
+% return : reflection_matrix_SO3__ : reflection transformation matrix SO3 
+    plane_normal_vec = vector_to_reflect_against_ ;      %  vector to reflect _along_ . The cross-product is the normal : in the xy-plane
+    plane_unit_normal_vec = plane_normal_vec./norm_2(plane_normal_vec,1) ; %  vector to reflect _along_ 
+    reflection_rot_SO3 = eye(3) - 2*(plane_unit_normal_vec*plane_unit_normal_vec') ; % Reflection SO3
+    reflection_rot_SE3 = r2t(reflection_rot_SO3) ; % Reflection SE3/Transform
+            % plot3_rows(mu','ks')
+            % A =  [ [ eye(3) -1*[ mu'] ]  ; [ 0 0 0 1 ] ]
+            % R_ = A*R_a*inv(A)
+            % plot3_rows(R_*euc2hom(points_3D_f1),'ms')
+    translation_to_point_of_reflection =  [ [ eye(3)  [ reflection_origin_] ]  ; [ 0 0 0 1 ] ] ; % Translation SE3/Transform
+    reflection_matrix_SO3__ = translation_to_point_of_reflection*reflection_rot_SE3*inv(translation_to_point_of_reflection) ; % Reflection SE3/Transform
+end
