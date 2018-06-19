220fa7558850c04f4bb36ddde076be28a15a21b5
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-06-19 13:59:05 +1000

    return the target point as well as the camera

--- camera_extrinsics__place_camera_safely_2.m
+++ camera_extrinsics__place_camera_safely_2.m
@@ -4,10 +4,10 @@ Generates a camera pose which has a minimum proportion of 3D points in the field
 -- generate camera pose with camera_extrinsics__place_camera_2()
 -- check the proportion of 3D points in the field of view 
 %}
-function camera__ = camera_extrinsics__place_camera_safely_2(min_angle_degs_,angle_range_degs_, x_max, y_max, z_max, camera_gauss_scale_, target_gauss_scale_, points_3D_,proportion_in_fov_)
+function [camera__ , target_point__ ] = camera_extrinsics__place_camera_safely_2(min_angle_degs_,angle_range_degs_, x_max, y_max, z_max, camera_gauss_scale_, target_gauss_scale_, points_3D_,proportion_in_fov_)
     while true
-        target_point = points_3D_(:,randi(size(points_3D_,2))) + (rand(([3,1]))-0.5).*target_gauss_scale_; 
-        camera__ = camera_extrinsics__place_camera_2(min_angle_degs_,angle_range_degs_, x_max, y_max, z_max, camera_gauss_scale_, target_point);
+        target_point__ = points_3D_(:,randi(size(points_3D_,2))) + (rand(([3,1]))-0.5).*target_gauss_scale_; 
+        camera__ = camera_extrinsics__place_camera_2(min_angle_degs_,angle_range_degs_, x_max, y_max, z_max, camera_gauss_scale_, target_point__);
         points_2D = camera__.project(points_3D_);
         num_points_in_FoV = sum( ...
             ( points_2D(1,:)>=camera__.limits(1) & ...
