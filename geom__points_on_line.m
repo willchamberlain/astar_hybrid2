248ebd05c2699c4611f59b350b5871c6583d341d
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-07-03 01:34:39 +1000

    calculate the grid-points on a line, for pathing: equivalent to for rendering a line to display pixels.

---
+++ geom__points_on_line.m
@@ -0,0 +1,54 @@
+% See http://www.it.uu.se/edu/course/homepage/grafik1/ht03/Lectures/LinePolygon/x_lines.htm
+function points_on_line__ = geom__points_on_line(Xstart, Ystart, Xend, Yend)
+
+%  line_spec = [ 100 140 ;
+%     100.1 140 ] 
+%  Xstart = line_spec(1,1)
+%  Ystart = line_spec(1,2)
+%  Xend = line_spec(2,1)
+%  Yend = line_spec(2,2)
+
+Xdifference = (Xend-Xstart)
+Ydifference = (Yend-Ystart)
+
+
+% figure; hold on; 
+index = 1 ;
+if 0 == Xdifference && 0 == Ydifference
+        % plot([Xstart], [Ystart],'r.')  % plot_pixel(x, y)
+        points_on_line__(index,:) = [Xstart,Ystart] ;
+        index=index+1
+elseif Xdifference ~= 0
+    y = Ystart
+    delta_Y = Ydifference / Xdifference
+    if (Ydifference < 0 && delta_Y >0) || (Ydifference > 0 && delta_Y < 0)
+        delta_Y = -delta_Y 
+    end
+    step = 1
+    if Xstart > Xend 
+        step = -1
+    end
+    for x = Xstart : step : Xend
+        %plot([x], [y],'r.')  % plot_pixel(x, y)
+        points_on_line__(index,:) = [x,y] ;
+        index=index+1        
+        y = y + delta_Y
+    end
+else % --> Ydifference ~= 0
+    x = Xstart
+    delta_X = Xdifference / Ydifference
+    if (Xdifference < 0 && delta_X >0) || (Xdifference > 0 && delta_X < 0)
+        delta_X = -delta_X 
+    end
+    step = 1
+    if Ystart > Yend 
+        step = -1
+    end
+    for y = Ystart : step : Yend
+        %plot([x], [y],'r.')  % plot_pixel(x, y)
+        points_on_line__(index,:) = [x,y] ;
+        index=index+1        
+        x = x + delta_X
+    end    
+end
+
