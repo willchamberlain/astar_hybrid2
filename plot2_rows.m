862e02f4afa82e957656e78e0fbde10f7e552bc4
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-07-03 01:36:39 +1000

    return the graphics object handle of the plotted object

--- plot2_rows.m
+++ plot2_rows.m
@@ -1,3 +1,4 @@
 function graphics_object_handle__ = plot2_rows(matrix_of_2D, varargin)
     graphics_object_handle__  =  plot(matrix_of_2D(1,:) , matrix_of_2D(2,:) , varargin{:});
+                                                     % plot3(matrix_of_3D(1,:) , matrix_of_3D(2,:) , matrix_of_3D(3,:), varargin{:})
 end
