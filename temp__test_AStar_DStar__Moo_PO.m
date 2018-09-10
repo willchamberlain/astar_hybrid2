93b452dfe585fc89df2dd4257c422d02cf4c1018
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-09-10 18:05:15 +1000

    Updating /mnt/nixbig/downloads/RobotciVisionToolbox-GIT/master/robotics-toolbox-matlab/astar/DstarMOO.m

--- temp__test_AStar_DStar__Moo_PO.m
+++ temp__test_AStar_DStar__Moo_PO.m
@@ -9,21 +9,22 @@ rmpath( '/mnt/nixbig/downloads/RobotciVisionToolbox-GIT/master/robotics-toolbox-
 %%
 %%
 
-map = zeros(100,100)  ;
+map = zeros(100,120)  ;
          goal = [50;30];
          goal = [40;90];
-         start = [20;10];
+         start = [120;10];
          % as = AstarMOO(map);    % create Navigation object
          % as = AstarPO(map);    % create Navigation object    %  Error - Undefined function or variable 'paretofront'
          as = DstarMOO(map);    % create Navigation object
          % as = DstarPO(map);    % create Navigation object    %  Error - Undefined function or variable 'paretofront'
-         cost_layer_2 = zeros(100,100)  ;
-         cost_layer_2(30:35,20:80) = 1;
-         as.addCost(1,cost_layer_2);        % add 1st add'l cost layer L
+         cost_layer_1 = zeros(100,120)  ;
+         cost_layer_1(30:35, 30:120) = 1;
+         as.addCost(1,cost_layer_1);        % add 1st add'l cost layer L
          
-         cost_layer_3 = zeros(100,100)  ;
-         cost_layer_3(70:75,20:80) = 1;
-         as.addCost(2,cost_layer_3);        % add 1st add'l cost layer L
+         cost_layer_2 = zeros(100,120)  ;
+         cost_layer_2(70:75,35:80) = 1;
+         as.addCost(2,cost_layer_2);        % add 1st add'l cost layer L        
+         
         
          as.plan(goal,2);       % setup costmap for specified goal
          as.path(start);        % plan solution path star-goal, animate
@@ -38,12 +39,12 @@ map = zeros(100,100)  ;
          % as = AstarPO(map);    % create Navigation object    %  Error - Undefined function or variable 'paretofront'
          as = DstarMOO(map);    % create Navigation object
          % as = DstarPO(map);    % create Navigation object    %  Error - Undefined function or variable 'paretofront'
-         cost_layer_2 = zeros(100,100)  ;         
-         cost_layer_2(30:35,20:80) = 1;
-         cost_layer_3 = zeros(100,100)  ;    
-         cost_layer_3(50:55,40:80) = 1;
-         as.addCost(1,cost_layer_2);        % add 1st add'l cost layer L
-         as.addCost(2,cost_layer_3);        % add 1st add'l cost layer L
+         cost_layer_1 = zeros(100,100)  ;         
+         cost_layer_1(30:35,20:80) = 1;
+         cost_layer_2 = zeros(100,100)  ;    
+         cost_layer_2(50:55,40:80) = 1;
+         as.addCost(1,cost_layer_1);        % add 1st add'l cost layer L
+         as.addCost(2,cost_layer_2);        % add 1st add'l cost layer L
         
          as.plan(goal,2);       % setup costmap for specified goal
          as.path(start);        % plan solution path star-goal, animate
