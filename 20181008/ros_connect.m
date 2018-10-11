92cb47dc1cec72bd994bf9768c66e5639754acca
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-10-11 15:19:39 +1000

    basics of connecting to ROS from Matlab

---
+++ 20181008/ros_connect.m
@@ -0,0 +1,32 @@
+%%
+
+getenv('ROS_MASTER_URI')
+getenv('ROS_HOSTNAME')
+getenv('ROS_IP')
+
+setenv('ROS_MASTER_URI','http://172.19.30.206:11311')
+setenv('ROS_IP','172.19.30.206')
+setenv('ROS_HOSTNAME','172.19.30.206')
+
+
+setenv('ROS_MASTER_URI','http://192.168.86.202:11311')
+setenv('ROS_IP','192.168.86.202')
+setenv('ROS_HOSTNAME','192.168.86.202')
+
+
+setenv('ROS_MASTER_URI','http://192.168.86.202:11311')
+setenv('ROS_IP','192.168.86.202')
+setenv('ROS_HOSTNAME','192.168.86.202')
+
+
+rosinit
+
+
+%%
+
+
+rosnode list
+rostopic list
+rosservice list
+
+tf_tree = rostf
