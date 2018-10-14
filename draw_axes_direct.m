1654c2389116feb4dc3e8fae79a38380c66d8f03
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-10-15 03:04:07 +1000

    pass on varargin

--- draw_axes_direct.m
+++ draw_axes_direct.m
@@ -19,13 +19,13 @@ camera_to_world(1:3,1:3) = se3_scaled;
     camera_axis_z = camera_to_world*[0 0 1.0*axis_arrow_scale 1]';
     
     if size(varargin) >= 2
-        plot3( [camera_position(1,1),camera_axis_u(1)] , [camera_position(2,1),camera_axis_u(2)] , [camera_position(3,1),camera_axis_u(3)] , 'Color', 'r', varargin)
-        plot3( [camera_position(1,1),camera_axis_v(1)] , [camera_position(2,1),camera_axis_v(2)] , [camera_position(3,1),camera_axis_v(3)] , 'Color', 'g', varargin)
-        plot3( [camera_position(1,1),camera_axis_z(1)] , [camera_position(2,1),camera_axis_z(2)] , [camera_position(3,1),camera_axis_z(3)] , 'Color', 'b', varargin)
+        plot3( [camera_position(1,1),camera_axis_u(1)] , [camera_position(2,1),camera_axis_u(2)] , [camera_position(3,1),camera_axis_u(3)] , 'Color', 'r',  varargin{:} )
+        plot3( [camera_position(1,1),camera_axis_v(1)] , [camera_position(2,1),camera_axis_v(2)] , [camera_position(3,1),camera_axis_v(3)] , 'Color', 'g',  varargin{:} )
+        plot3( [camera_position(1,1),camera_axis_z(1)] , [camera_position(2,1),camera_axis_z(2)] , [camera_position(3,1),camera_axis_z(3)] , 'Color', 'b',  varargin{:} )
     else 
-        plot3( [camera_position(1,1),camera_axis_u(1)] , [camera_position(2,1),camera_axis_u(2)] , [camera_position(3,1),camera_axis_u(3)] , 'Color', 'r')
-        plot3( [camera_position(1,1),camera_axis_v(1)] , [camera_position(2,1),camera_axis_v(2)] , [camera_position(3,1),camera_axis_v(3)] , 'Color', 'g')
-        plot3( [camera_position(1,1),camera_axis_z(1)] , [camera_position(2,1),camera_axis_z(2)] , [camera_position(3,1),camera_axis_z(3)] , 'Color', 'b')
+        plot3( [camera_position(1,1),camera_axis_u(1)] , [camera_position(2,1),camera_axis_u(2)] , [camera_position(3,1),camera_axis_u(3)] , 'Color', 'r',  varargin{:} )
+        plot3( [camera_position(1,1),camera_axis_v(1)] , [camera_position(2,1),camera_axis_v(2)] , [camera_position(3,1),camera_axis_v(3)] , 'Color', 'g',  varargin{:} )
+        plot3( [camera_position(1,1),camera_axis_z(1)] , [camera_position(2,1),camera_axis_z(2)] , [camera_position(3,1),camera_axis_z(3)] , 'Color', 'b',  varargin{:} )
     end
     
     
