21ae490e7dcee15a13cc0158f16d15ea027a26a2
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2018-10-22 12:24:43 +1000

    Example to play and edit video on Ubunutu.

---
+++ utils_desktop/edit_video_on_ubuntu.m
@@ -0,0 +1,10 @@
+%{
+https://au.mathworks.com/help/images/ref/montage.html
+
+https://au.mathworks.com/matlabcentral/answers/336730-cut-out-scenes-from-a-video
+http://areshmatlab.blogspot.com/2010/02/matlab-my-basic-structure-for-editing.html
+%}
+
+implay(
+fin = '/mnt/nixbig/ownCloud/documents/PhD_Thesis_test_Texstudio/videos_and_experiments/plan_to_observe_2b_2018_10_09_1052-simplescreenrecorder-2018-10-09_10.24.32.mp4'
+fout = '/mnt/nixbig/ownCloud/documents/PhD_Thesis_test_Texstudio/videos_and_experiments/plan_to_observe_2b_2018_10_09_1052-simplescreenrecorder-2018-10-09_10.24.32_trimmed.mp4'
